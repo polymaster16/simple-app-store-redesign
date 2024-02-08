ae7e45999c2b988925e904bd7ffe13d80ab9b23f
polymaster16 <uili.kam8@gmail.com>
2024-02-08 19:09:13 +0100

    modified:   app.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,11 @@
 <template>
+     <Head>
+      <Title>
+         Simple App Store
+      </Title>
+      <Link rel="icon" :href="`https://cdn.midjourney.com/100de0e8-bce3-416c-a677-bb0aef0878fb/0_2.webp`"/>
+
+   </Head>
   <div v-if="mainstore.state" >
 <NuxtLayout>
   <NuxtPage>
